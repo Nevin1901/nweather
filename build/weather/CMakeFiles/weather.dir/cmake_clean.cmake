7c728383b76e7f6cfe581c69904897122f20230c
Nevin1901 <nevinpuri1901@gmail.com>
2021-02-25 15:47:57 +0100

    edited gitignore

---
+++ build/weather/CMakeFiles/weather.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/weather.dir/weather.cpp.o"
+  "libweather.a"
+  "libweather.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/weather.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
